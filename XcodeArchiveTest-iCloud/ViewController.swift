0a67d2ab7f791a6c5ef2f072aa6f25a62e834626
Akos Birmacher <akos.birmacher@bitrise.io>
2018-06-18 14:56:47 +0200

    Initial Commit

---
+++ XcodeArchiveTest-iCloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeArchiveTest-iCloud
+//
+//  Created by Akos Birmacher on 2018. 06. 18..
+//  Copyright © 2018. Akos Birmacher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
